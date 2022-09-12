9fe1f18fa7726c9a80d27a8891e62afac7e75ece
Santeri Pulkkinen <santeri.pulkkinen@centria.fi>
2022-09-12 20:52:02 +0300

    added something to test github error

--- src/App.js
+++ src/App.js
@@ -10,14 +10,10 @@ const App = () => {
       setBlogs( blogs )
     )  
   }, [])
-<<<<<<< HEAD
-// vaiha branch Main to main ja delete Main :D hf
-=======
 
->>>>>>> 618564989fbcc771ad1da9b6c36ccdef580b3623
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>blogs TEST changes for git hub</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
